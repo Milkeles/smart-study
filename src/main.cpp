bf37f62ea1670df1453d3442de80856ffaca779d
Hristo Hristov <milkeles.apps@gmail.com>
2025-11-05 23:59:32 +0200

    Fix: Added resources to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
 
   // Load global stylesheet
-  QFile styleFile("resources/styles/main.qss");
+  QFile styleFile(":/resources/styles/main.qss");
   if (styleFile.open(QFile::ReadOnly)) {
     QString style = QLatin1String(styleFile.readAll());
     app.setStyleSheet(style);
